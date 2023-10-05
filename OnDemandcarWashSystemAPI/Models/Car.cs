6233ef5b92a92acabc72017bdf04956cff8a0f60
Shlok B. Gupta <gshlok555@outlook.com>
2023-10-05 22:53:38 +0530

    Some Model classes added

---
+++ OnDemandcarWashSystemAPI/Models/Car.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace OnDemandcarWashSystemAPI.Models
+{
+	public class Car
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = "Car Model is required")]
+		[DisplayName("Car Model")]
+		public string? CarModel { get; set; }
+
+		[Required(ErrorMessage = "Car Type is required")]
+		[DisplayName("Car Type")]
+		public string? CarType { get; set; }
+
+		[DisplayName("Car Image")]
+		public string? CarImg { get; set; }
+
+		public int UserId { get; set; }
+	}
+}
---
+++ OnDemandcarWashSystemAPI/Models/WashPackage.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace OnDemandcarWashSystemAPI.Models
+{
+	public class WashPackage
+	{
+		[Key]
+		public int Id { get; set; }
+		public string? Name { get; set; }
+		public string? Description { get; set; }
+		public double Price { get; set; }
+
+		[JsonIgnore]
+		public IEnumerable<AddOn>? AddOn { set; get; }
+	}
+}
